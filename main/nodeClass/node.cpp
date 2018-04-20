c3876d81c9bed39c85b94c13bd13de1bcd51892c
MikiEEE <mikemp1997@gmail.com>
2018-04-19 23:09:30 -0500

    Organized the node class

--- main/nodeClass/node.cpp
+++ main/nodeClass/node.cpp
@@ -1,1 +1,35 @@
+#include "node.hpp"
 
+node::node(node* init_link) {
+    link_field = init_link;
+}
+
+void node::set_link(node* new_link) {
+    link_field = new_link;
+    return;
+}
+
+node* node::link( ){
+    return link_field;
+}
+
+node* node::link( ) const {
+    return link_field;
+}
+
+void node::setLink(node *next_ptr ){
+    link_field = next_ptr;
+    return;
+}
+
+void node:: task() {
+    return;
+}
+
+int node::getReady() {
+    return 0;
+}
+
+int node::getPriority() {
+    return 0;
+}
--- main/nodeClass/node.hpp
+++ main/nodeClass/node.hpp
@@ -12,41 +12,15 @@ using namespace std;
 class node {
     public:
         typedef int value_type;
-        node(node* init_link = NULL) {
-            link_field = init_link;
-        }
-        
+        node(node* init_link = NULL);
         virtual  ~node() {}
-        
-
-        void set_link(node* new_link) {
-            link_field = new_link;
-        }
-        
-        node* link( ){
-            return link_field;
-        }
-    
-        node* link( ) const {
-            return link_field;
-        }
-
-        void setLink(node *next_ptr ){
-            link_field = next_ptr;
-        }
-        
-        virtual void task() {
-            return;
-        }
-
-        virtual int getReady() {
-            return 0;
-        }
-
-        virtual int getPriority() {
-            return 0;
-        }
-
+        void set_link(node* new_link);
+        node* link();
+        node* link() const;
+        void setLink(node *next_ptr);
+        virtual void task();
+        virtual int getReady();
+        virtual int getPriority();
     protected:
         node* link_field = NULL;
 };
